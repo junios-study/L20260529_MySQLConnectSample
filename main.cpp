0fbc9ff649616bea39400307c831cf767b2c7f66
junios-study <junios@naver.com>
2026-05-29 15:49:20 +0900

    디비로 아이디 비번 검색

--- main.cpp
+++ main.cpp
@@ -22,34 +22,46 @@ int main()
 	{
 		sql::Driver* MyDriver; //workbench
 		sql::Connection* MyConnection; //접속 정보
-		sql::Statement* MyStatement; //쿼리 창
+		//sql::Statement* MyStatement; //쿼리 창
 		sql::ResultSet* MyResultSet; //결과 창
+		sql::PreparedStatement* MyPreparedStatement; //쿼리를 만들때 injection 방어함.
 
 		MyDriver = get_driver_instance();
 		MyConnection = MyDriver->connect("tcp://127.0.0.1", "junios", "qweasd123");
 
 		MyConnection->setSchema("membership");
-		MyStatement = MyConnection->createStatement();
-		sql::SQLString Query = "select * from user where `user_id` = '";
-		Query = Query + UserID + "' and `user_pw` = sha2('";
-		Query = Query + Password + "', 512) and is_deleted = 'N';";
+		//MyStatement = MyConnection->createStatement();
+		//sql::SQLString Query = "select * from user where `user_id` = '";
+		//Query = Query + UserID + "' and `user_pw` = sha2('";
+		//Query = Query + Password + "', 512) and is_deleted = 'N';";
+		//MyResultSet = MyStatement->executeQuery(Query);
+
+		sql::SQLString Query = "select * from user where `user_id` = ? and `user_pw` = sha2( ?, 512) and is_deleted = 'N';";
+
+		MyPreparedStatement = MyConnection->prepareStatement(Query);
+		MyPreparedStatement->setString(1, UserID);
+		MyPreparedStatement->setString(2, Password);
+		MyResultSet = MyPreparedStatement->executeQuery();
 
 		std::cout << Query << std::endl;
 
-		MyResultSet = MyStatement->executeQuery(Query);
-
-		for (; MyResultSet->next();)
+		if (MyResultSet->rowsCount() == 0)
 		{
-			std::cout << MyResultSet->getInt("idx") << std::endl;
-			std::cout << MyResultSet->getString("user_id") << std::endl;
-			std::cout << MyResultSet->getString("user_pw") << std::endl;
-			std::cout << MyResultSet->getString("name") << std::endl;
-			std::cout << MyResultSet->getInt("is_deleted") << std::endl;
-			std::cout << MyResultSet->getString("created_at") << std::endl;
+			std::cout << "아이디 비번이 틀립니다.";
 		}
-
-
-
+		else
+		{
+			//key 설정
+			for (; MyResultSet->next();)
+			{
+				std::cout << MyResultSet->getInt("idx") << std::endl;
+				std::cout << MyResultSet->getString("user_id") << std::endl;
+				std::cout << MyResultSet->getString("user_pw") << std::endl;
+				std::cout << MyResultSet->getString("name") << std::endl;
+				std::cout << MyResultSet->getInt("is_deleted") << std::endl;
+				std::cout << MyResultSet->getString("created_at") << std::endl;
+			}
+		}
 	}
 	catch (sql::SQLException Exception)
 	{
